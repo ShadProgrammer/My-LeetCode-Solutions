ae32625b6b7ef41a938919236ed2aa697443137e
Shad_Programmer <dweepbariya2006@gmail.com>
2026-07-28 22:49:03 +0530

    Binary Search

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        if (nums.length == 1){
+            return nums[0]; 
+        }
+        if (nums[0] != nums[1]){
+            return nums[0];
+        }
+        if (nums[nums.length-2] != nums[nums.length-1]){
+            return nums[nums.length-1];
+        }
+        
+        int low = 1; int high = nums.length-2;
+
+        while (low <= high){
+            int mid = (low + high)/2;
+
+            if (nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            if ((mid%2 == 1 && nums[mid] == nums[mid-1]) || (mid%2 == 0 && nums[mid] == nums[mid+1])){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return high;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
